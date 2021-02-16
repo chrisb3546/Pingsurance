51bb222eedf4debe0a2fcbff9f10f7cf5274216c
chrisb3546 <chrisbradycode@gmail.com>
2021-02-16 10:47:55 -0500

    add bootstrap to application

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,2 +1,24 @@
 class LeadsController < ApplicationController
+    before_action :find_lead, only: [:edit, :update, :destroy] 
+    def index
+        @leads = Lead.all
+    end
+
+    def new
+        @lead = Lead.new
+    end
+
+    def create
+        @lead = Lead.new(lead_params)
+
+    end
+
+    private
+    def lead_params
+        params.require(:lead).permit(:first_name, :last_name, :phone_number, :note)
+    end
+
+    def find_lead
+        @lead = lead.find_by(id: params[:id])
+    end
 end

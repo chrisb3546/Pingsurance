7315615c32b1770036cecb7d1c7674221ab7a35c
chrisb3546 <chrisbradycode@gmail.com>
2021-02-16 11:54:32 -0500

    seed db and add UI functionality to lead table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+leads = [
+    ["John", "Doe", "123456789","hello"],
+    ["Jane", "Doe", "123456788","hello"],
+    ["test", "lead", "123456787","hello"]
+]
+
+leads.each do |first_name, last_name, phone_number, note|
+    Lead.create(first_name: first_name, last_name: last_name, phone_number: phone_number, note: note)
+end
+
+

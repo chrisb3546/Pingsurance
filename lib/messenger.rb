11df3befa83dcabf35a48321c5e1ff6d6dff5767
chrisb3546 <chrisbradycode@gmail.com>
2021-02-23 09:38:10 -0500

    remove line of code used for debugging in messenger.rb

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -5,7 +5,6 @@ module Messenger
         account_sid = ENV['TWILIO_SID']
         auth_token = ENV['TWILIO_AUTH_TOKEN']
 
-        hello = 'hello'
         
         @client = Twilio::REST::Client.new account_sid, auth_token
 

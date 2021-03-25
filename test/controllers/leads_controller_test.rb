3af060fc245fe849005838c8ca66b375e6c9ec48
chrisb3546 <chrisbradycode@gmail.com>
2021-03-25 14:16:21 -0400

    add unit testing for leads controller #index

--- test/controllers/leads_controller_test.rb
+++ test/controllers/leads_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class LeadsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @lead = Lead.new(first_name: "Joe", last_name: "Schmo", phone_number: "5555555555", note: "Thank you for choosing Pingsurance" )
+  end
+
+  test "should get index" do 
+    get leads_url
+    assert_response :success
+  end
 end
